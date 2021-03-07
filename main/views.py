a1f342af2d32b75a0c0dbc831670d5bf391a0ad2
nedasadri <nedasadri95@gmail.com>
2021-03-07 17:44:05 +0330

    fifth

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout
 from django.contrib.auth import login
+from . import models
 
 def home(request):
     if request.method == 'POST':
@@ -16,7 +17,26 @@ def home(request):
     return render(request, 'home.html')
 
 def teacher(request):
-    return render(request, 'teacher.html')
+    tamrinha = models.Tamrin.objects.all()
+    if request.method == 'POST':
+        matn = request.POST.get('matn')
+        time = request.POST.get('time')
+        tamrin = models.Tamrin(matn = matn, time = time)
+        tamrin.save()
+
+    return render(request, 'teacher.html', context = {'tamrinha':tamrinha})
 
 def student(request):
-    return render(request, 'student.html')
+    tamrinha = models.Tamrin.objects.all()
+    info = []
+    for tamrin in tamrinha:
+        Ersal = None
+        for ersal in tamrin.ersalha.all():
+            if ersal.student == request.user:
+                Ersal = ersal
+                break
+        info.append( [homework, upload] )
+
+    filmha = models.Film.objects.all()
+
+    return render(request, 'student.html', context = {'tamrinha':info, 'filmha':filmha})

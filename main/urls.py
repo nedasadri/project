faedb9280f0012a03656a2e3c1941d6f678e057b
nedasadri <nedasadri95@gmail.com>
2021-03-07 18:25:52 +0330

    sixth

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 app_name = 'main'
@@ -6,4 +9,8 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('teacher/', views.teacher, name = 'teacher'),
     path('student/', views.student, name = 'student'),
+    path('filmha/', views.ersal_film, name = 'ersal_film'),
+    path('nemayesh/<int:pk>/', views.nemayesh, name = 'nemayesh'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout
 from django.contrib.auth import login
+from django import forms
 from . import models
 
 def home(request):
@@ -35,8 +36,34 @@ def student(request):
             if ersal.student == request.user:
                 Ersal = ersal
                 break
-        info.append( [homework, upload] )
+        info.append( [tamrin, Ersal] )
 
     filmha = models.Film.objects.all()
 
     return render(request, 'student.html', context = {'tamrinha':info, 'filmha':filmha})
+
+
+
+
+class FilmForm(forms.ModelForm):
+    class Meta:
+        model = models.Film
+        fields = '__all__'
+        widgets = {
+            'file': forms.FileInput(attrs={'accept': 'video/*'}),
+        }
+
+def ersal_film(request):
+
+    film_form = FilmForm()
+    if request.method == 'POST':
+        form = FilmForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    filmha = models.Film.objects.all()
+    return render(request, 'films.html', context = {'filmha':filmha, 'film_form':film_form})
+
+
+def nemayesh(request, pk):
+    film = models.Film.objects.get(pk = pk)
+    return render(request, 'nemayesh.html', context = {'film':film})

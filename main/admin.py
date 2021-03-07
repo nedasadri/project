baa5f37ec2223133b53c62852fa6770b0cf1dc5e
nedasadri <nedasadri95@gmail.com>
2021-03-07 16:23:15 +0330

    second

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    list_display = ('username', 'teacher')
+    list_filter = ()
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'teacher', 'code')}),)
+
+    ordering = ('username',)
+    filter_horizontal = ()
+
+admin.site.register(models.MyUser, MyUserAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from django.utils import timezone
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+
+    def create_user(self, username, password = None):
+        user = self.model(username = username)
+
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+
+    def create_superuser(self, username, password = None):
+
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+        return user
+
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+
+    username =  models.EmailField(unique = True)
+    teacher = models.BooleanField(default = False)
+    code = models.DecimalField(max_digits = 10, decimal_places = 0, default = 0)
+
+    is_staff = models.BooleanField( default = False)
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
